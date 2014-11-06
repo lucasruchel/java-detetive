5d703b76b7b6c01c03ee8055f408fc92b74f3de9
Lucas Ruchel <lukasruchel@gmail.com>
2014-11-06 09:58:00 -0200

    comeco

--- src/game/Pista.java
+++ src/game/Pista.java
@@ -9,15 +9,24 @@ public class Pista extends MyComponent
 {
 	private Dica dica;
 	
-	public void addDicas(Dica dica){
+	public void addDicas(String informacoes){
 		if(this.dica == null)
-			
+			this.dica = new Dica(this.x+50, this.y, this.width, this.height); 
+			this.dica.setDica(informacoes);
 	}
-	
+	@Override
+	public boolean isOver(int x, int y) {
+		if(x> this.x && x < this.x+this.width && y>this.y && y< this.y + this.height){
+			this.dica.setSelected(true);
+			return true;
+		}
+		return false;
+		
+	}
 	@Override
 	public void paint(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		if (this.dica.isSelected())
+			this.dica.paint(g);
 	}
 
 }
